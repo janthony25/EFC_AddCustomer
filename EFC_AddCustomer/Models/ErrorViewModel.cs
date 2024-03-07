36b804f8cf1ac4431fe60f5238b3461b4331d974
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 17:10:35 +1300

    Add project files.

---
+++ EFC_AddCustomer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EFC_AddCustomer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
