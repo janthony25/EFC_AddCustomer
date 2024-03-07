c5386d06cc4ed06e51f2dc6a2d713364b3546f04
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 17:22:01 +1300

    Add Default ConnectionString

---
+++ EFC_AddCustomer/Controllers/InvoiceController.cs
@@ -0,0 +1,20 @@
+﻿using EFC_AddCustomer.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFC_AddCustomer.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public InvoiceController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
