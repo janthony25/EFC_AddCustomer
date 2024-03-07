c204ba6695be9db01c8b04802bb97aa42b3dd039
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 17:18:44 +1300

    Add ApplicationDbContext

---
+++ EFC_AddCustomer/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using EFC_AddCustomer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFC_AddCustomer.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CustomerName> CustomerNames { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .HasOne<CustomerName>(i => i.CustomerName)
+                .WithMany(c => c.Items)
+                .HasForeignKey(i => i.CustomerName)
+                .IsRequired();
+        }
+    }
+}
