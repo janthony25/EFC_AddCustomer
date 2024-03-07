47e8a3f6030ca8892eeec13b13934eddb40bdff6
Jun Anthony Calupcupan <junanthony.business@gmail.com>
2024-03-07 17:12:50 +1300

    Add Customer and Item Model

---
+++ EFC_AddCustomer/Models/CustomerName.cs
@@ -0,0 +1,10 @@
+﻿namespace EFC_AddCustomer.Models
+{
+    public class CustomerName
+    {
+        public int CustomerId { get; set; }
+        public CustomerName Name { get; set; }
+
+        public ICollection<Item> Items { get; set; }
+    }
+}
---
+++ EFC_AddCustomer/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace EFC_AddCustomer.Models
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string ItemPrice { get; set; }
+
+        public int CustomerId { get; set; }
+        public CustomerName CustomerName { get; set; }
+    }
+}
